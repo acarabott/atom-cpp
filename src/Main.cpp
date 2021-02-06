f5a1237feaf3cd7e55ec7d18613a97c772b2c773
Arthur Carabott <arthur@arthurcarabott.com>
2021-02-05 22:37:16 -0800

    print state

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
+struct State {
+    int count = 0;
+};
+
+void printState(const State& state)
+{
+    std::cout << "State:" << std::endl;
+    std::cout << "count: " << state.count <<  std::endl;
+}
+
 int main(int argc, char *argv[]) {
-  std::cout << "hello world" << std::endl;
+
+    State state;
+
+    printState(state);
 }
