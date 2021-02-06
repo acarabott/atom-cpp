3cb461044105308e88ec07fcdf36a7396f606fe8
Arthur Carabott <arthur@arthurcarabott.com>
2021-02-06 00:32:11 -0800

    subscribe

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
+#include <vector>
 
 template<class T>
 class Atom {
 public:
+    using Update = std::function<void(T &value)>;
+
     const T get() { return value; }
 
-    void set(const T &value_) { value = value_; }
+    void set(const T &value_) {
+        value = value_;
+        for (const auto &sub : subs) {
+            sub(value);
+        }
+    }
+
+    void subscribe(const Update update) {
+        subs.push_back(update);
+    }
 
 private:
     T value;
+
+    std::vector<const Update> subs;
 };
 
 struct State {
@@ -35,5 +49,13 @@ int main(int argc, char *argv[]) {
     std::cout << "New: " << std::endl;
     printState(state);
     std::cout << std::endl;
+
+    db.subscribe([](State& newState) {
+        std::cout << "Update!" << std::endl;
+
+        printState(newState);
+    });
+
+    db.set(state);
 
 }
