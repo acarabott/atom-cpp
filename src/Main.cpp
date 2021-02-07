1b79ee578a76df44688b500e10a4f9a015326aef
Arthur Carabott <arthur@arthurcarabott.com>
2021-02-06 21:14:06 -0800

    cursor basics

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,43 +4,68 @@
 template<class T>
 class Atom {
 public:
-    using Update = std::function<void(T &value)>;
+    using Subscription = std::function<void(T &value)>;
 
     const T get() const { return value; }
 
     void set(const T value_) {
-        std::cout << "set" << std::endl;
-        std::cout << "before" << std::endl;
-        print();
-
         value = value_;
-
-        std::cout << "after" << std::endl;
-        print();
-        std::cout << std::endl;
 
         for (const auto &sub : subs) {
             sub(value);
         }
     }
 
-    void subscribe(const Update &update) {
-        subs.push_back(update);
-    }
-
-    void print() const {
-        if (printValue != nullptr) {
-            printValue(value);
+    void update(std::function<T(const T value)> update) {
+        if (update != nullptr) {
+            set(update(get()));
         }
     }
 
-    std::function<void(const T &value)> printValue;
-
+    void subscribe(const Subscription &update) {
+        subs.push_back(update);
+    }
 
 private:
     T value;
 
-    std::vector<const Update> subs;
+    std::vector<const Subscription> subs;
+};
+
+template<class T_State, class T_Value>
+class Cursor {
+    using Get = std::function<T_Value(T_State)>;
+    using Set = std::function<void(T_Value, T_State &)>;
+    using Subscription = std::function<void(T_Value &value)>;
+public:
+    Cursor(Atom<T_State> &atom, Get get, Set set)
+            : mAtom(atom), mGet(get), mSet(set) {}
+
+    T_Value get() {
+        return mGet(mAtom.get());
+    }
+
+    void set(T_Value value) {
+        auto state = mAtom.get();
+        mSet(value, state);
+        mAtom.set(state);
+
+        for (const auto &sub : subs) {
+            sub(value);
+        }
+    }
+
+    void subscribe(const Subscription &subscriber) {
+        subs.push_back(subscriber);
+    }
+
+    ~Cursor() {};
+
+private:
+    Atom<T_State> &mAtom;
+    Get mGet = nullptr;
+    Set mSet = nullptr;
+    std::vector<const Subscription> subs;
 };
 
 struct State {
@@ -48,16 +73,17 @@ struct State {
 };
 
 void increment(Atom<State> &db) {
-    auto state = db.get();
-    std::cout << "increment: " << state.count << std::endl;
-    state.count++;
-    db.set(state);
+    db.update([](State state) {
+        state.count++;
+        return state;
+    });
 }
 
 void decrement(Atom<State> &db) {
-    auto state = db.get();
-    state.count--;
-    db.set(state);
+    db.update([](State state) {
+        state.count--;
+        return state;
+    });
 }
 
 int getCount(Atom<State> &db) {
@@ -72,12 +98,8 @@ int main(int argc, char *argv[]) {
 
     Atom<State> db;
 
-    db.printValue = [](const State &state) {
-        printState(state);
-    };
-
     db.subscribe([](State newState) {
-        std::cout << "Update!" << std::endl;
+        std::cout << "Subscription!" << std::endl;
 
         printState(newState);
         std::cout << std::endl;
@@ -87,5 +109,18 @@ int main(int argc, char *argv[]) {
     increment(db);
     decrement(db);
 
-    std::cout << "final count: " << getCount(db) << std::endl;
+    std::cout << "final count: " << getCount(db) << std::endl << std::endl;
+
+    const auto getCount = [](State state) { return state.count; };
+    const auto setCount = [](int value, State &state) { state.count = value; };
+
+    Cursor<State, int> countCursor(db, getCount, setCount);
+
+    std::cout << "cursor: " << countCursor.get() << std::endl;
+
+    countCursor.subscribe([](int count) {
+        std::cout << "cursor sub: " << count << std::endl;
+    });
+
+    countCursor.set(6);
 }
